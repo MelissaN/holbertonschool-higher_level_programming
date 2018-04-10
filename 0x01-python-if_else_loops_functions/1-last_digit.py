8d160835dbe65adc2d8909f539b72e53f5ebdb57
MelissaN <majajachiu@yahoo.com>
2018-04-10 20:12:19 +0000

    fix pep8 style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,11 @@ else:
 
 remainder = pos % 10
 if remainder > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, remainder))
+    print("Last digit of {} is {} and is greater than 5"
+          .format(number, remainder))
 elif remainder is 0:
-    print("Last digit of {} is {} and is 0".format(number, remainder))
+    print("Last digit of {} is {} and is 0"
+          .format(number, remainder))
 else:
-    print("Last digit of {} is {} and is less than 6 and is not 0".format(number, remainder))
+    print("Last digit of {} is {} and is less than 6 and is not 0"
+          .format(number, remainder))
