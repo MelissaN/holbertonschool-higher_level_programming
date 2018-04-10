12b031789f6419bb01491ca6e2ece6761f36f531
MelissaN <majajachiu@yahoo.com>
2018-04-10 20:44:59 +0000

    initial commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range((i+1), 10):
+        if (i is not 8) or (j is not 9):
+            print("{}{}, ".format(i, j), end='')
+        else:
+            print("{}{}".format(i, j))
