ed910c450c03a1da7d271924617f8648245b710b
MelissaN <majajachiu@yahoo.com>
2018-04-10 02:59:44 +0000

    remove unnecessary ptr, refactor

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,21 +7,16 @@
  */
 int check_cycle(listint_t *list)
 {
-	listint_t *tmp_fast = NULL;
-	listint_t *fast = NULL;
-	listint_t *slow = NULL;
+	listint_t *fast = list;
+	listint_t *slow = list;
 
 	if (!list)
 		return (0);
 
-	fast = list;
-	slow = list;
-
 	while (1)
 	{
 		/*traverse through nodes as long as linked list node exists*/
-		tmp_fast = fast;
-		if (tmp_fast->next != NULL && tmp_fast->next->next != NULL)
+		if (fast->next != NULL && fast->next->next != NULL)
 		{
 			fast = fast->next->next;
 			slow = slow->next;
