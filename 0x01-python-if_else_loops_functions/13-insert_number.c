cbb1dbffe0deaa90149e53e72a7ad101cce393b9
MelissaN <majajachiu@yahoo.com>
2018-04-10 23:46:15 +0000

    initial commit

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+/**
+ * insert_node - malloc and insert node into sorted singly linked list
+ * @head: pointer to head of linked list
+ * @number: data for new node
+ * Return: address of new node, or NULL if failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *tmp = NULL;
+	listint_t *new = NULL;
+
+	if (!head)
+		return (NULL);
+
+	/* malloc new node */
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+
+	/* if no linked list, insert node as the only member */
+	if (*head == NULL)
+	{
+		*head = new;
+		(*head)->next = NULL;
+		return (new);
+	}
+	/* if only one node in linked list, do comparision and insert */
+	if ((*head)->next == NULL)
+	{
+		if ((*head)->n < new->n)
+			(*head)->next = new;
+		else
+		{
+			new->next = *head;
+			*head = new;
+		}
+		return (new);
+	}
+
+	/* if lots of nodes in linked list, do comparision and insert */
+	tmp = *head;
+	while (tmp->next != NULL)
+	{
+		/* if new node num is the same as an existing node, insert */
+		/* compare previous node and next node, insert in between */
+		if (((new->n > tmp->n) && (new->n < (tmp->next)->n)) ||
+		    (new->n == tmp->n))
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		tmp = tmp->next;
+	}
+	/* if new node is greatest and never inserted, insert now */
+	tmp->next = new;
+	return (new);
+}
