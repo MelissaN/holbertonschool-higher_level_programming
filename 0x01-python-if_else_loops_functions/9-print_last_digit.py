da5258a72e885792c2bc4c35393565ca8316cfb7
MelissaN <majajachiu@yahoo.com>
2018-04-10 22:05:43 +0000

    undo variable

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 def print_last_digit(number):
     if number < 0:
-        number = number % (-10)
-        n = number * -1
-    else:
-        n = number % 10
-
-    print("{}".format(n), end='')
-    return (n)
+        number = number * -1
+    print("{}".format(number % 10), end='')
+    return (number % 10)
