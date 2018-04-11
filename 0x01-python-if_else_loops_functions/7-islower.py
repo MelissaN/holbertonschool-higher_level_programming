34fbb0c256feb1fe801aa0ecc00ef9b50fb99e70
MelissaN <majajachiu@yahoo.com>
2018-04-11 23:06:52 +0000

    fix pep8 style

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+def islower(c):
+    return (ord('a') <= ord(c) <= ord('z'))
+
 #if (ord(c) >= ord('a')) and (ord(c) <= ord('z')):
 #        return True
 #    else:
 #        return False
-
-def islower(c):
-    return (ord('a') <= ord(c) <= ord('z'))
