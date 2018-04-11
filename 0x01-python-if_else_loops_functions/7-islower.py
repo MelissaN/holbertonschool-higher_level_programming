82737e7ff1642e03c9a278e028b83e94cfe6fc46
MelissaN <majajachiu@yahoo.com>
2018-04-11 23:04:21 +0000

    add another way

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+#if (ord(c) >= ord('a')) and (ord(c) <= ord('z')):
+#        return True
+#    else:
+#        return False
+
 def islower(c):
-    if (ord(c) >= ord('a')) and (ord(c) <= ord('z')):
-        return True
-    else:
-        return False
+    return (ord('a') <= ord(c) <= ord('z'))
