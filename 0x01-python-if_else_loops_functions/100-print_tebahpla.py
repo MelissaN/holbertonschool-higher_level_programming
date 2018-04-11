7455b36a9b9a014eeed5ab7572140c09705b967f
MelissaN <majajachiu@yahoo.com>
2018-04-11 00:43:35 +0000

    undo global variable

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-i = 0
 for n in range(ord('z'), ord('a')-1, -1):
-    if i % 2 == 0:
+    if n % 2 == 0:
         print("{}".format(chr(n)), end='')
     else:
         print("{}".format(chr(n - ord('a') + ord('A'))), end='')
-    i += 1
