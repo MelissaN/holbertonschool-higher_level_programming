a8850571d2ca641096582ef2a4616cb9f25bd621
MelissaN <majajachiu@yahoo.com>
2018-04-11 22:58:49 +0000

    make quotations consistent

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for c in range(ord('a'), ord('z')+1):
-    print("{}".format(chr(c)), end='')
+    print('{}'.format(chr(c)), end='')
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i is not 99:
-        print("{:02}, ".format(i), end='')
+        print("{:02}, ".format(i), end="")
     else:
         print("{:02}".format(i))
