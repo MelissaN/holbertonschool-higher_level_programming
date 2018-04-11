be7c3874dc1ba28d3edacad7d30c72a5455d28da
MelissaN <majajachiu@yahoo.com>
2018-04-11 22:54:51 +0000

    revert to original way

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -6,7 +6,3 @@ for i in range(0, 26):
     else:
         c = chr(c)
     print("{}".format(c), end='')
-
-# alternative method:
-# for i in range(ord('z'), ord('a')-1, -1):
-#     print('{:c}'.format(i) if i % 2 == 0 else chr(i-32), end='')
