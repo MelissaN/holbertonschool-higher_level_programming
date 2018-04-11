20126a4524f5056a1ab8965eadf8294d0a1b9774
MelissaN <majajachiu@yahoo.com>
2018-04-11 22:25:26 +0000

    another way using ternary

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-for i in range(0, 26):
-    c = ord('z') - i
-    if i % 2 == 1:
-        c = (chr(c - ord('a') + ord('A')))
-    else:
-        c = chr(c)
-    print("{}".format(c), end='')
+for i in range(ord('z'), ord('a')-1, -1):
+    print('{:c}'.format(i) if i % 2 == 0 else chr(i-32), end='')
+
+
+#for i in range(0, 26):
+#    c = ord('z') - i
+#    if i % 2 == 1:
+#        c = (chr(c - ord('a') + ord('A')))
+#    else:
+#        c = chr(c)
+#    print("{}".format(c), end='')
