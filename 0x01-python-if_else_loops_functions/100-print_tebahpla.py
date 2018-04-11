76e4e4922bac3b895bef3f8af26180bd021499b3
MelissaN <majajachiu@yahoo.com>
2018-04-11 00:38:33 +0000

    print with string format

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,7 @@
 i = 0
 for n in range(ord('z'), ord('a')-1, -1):
     if i % 2 == 0:
-        print(chr(n), end='')
+        print("{}".format(chr(n)), end='')
     else:
-        print(chr(n - ord('a') + ord('A')), end='')
+        print("{}".format(chr(n - ord('a') + ord('A'))), end='')
     i += 1
