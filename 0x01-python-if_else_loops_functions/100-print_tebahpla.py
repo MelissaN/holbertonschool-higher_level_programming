99fc6756d3cee7339f4a3f6fb31f5e31dcae6f81
MelissaN <majajachiu@yahoo.com>
2018-04-11 01:02:31 +0000

    alternative method to limit prints

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for n in range(ord('z'), ord('a')-1, -1):
-    if n % 2 == 0:
-        print("{}".format(chr(n)), end='')
+for i in range(0,26):
+    c = ord('z') - i
+    if i % 2 == 1:
+        c = (chr(c - ord('a') + ord('A')))
     else:
-        print("{}".format(chr(n - ord('a') + ord('A'))), end='')
+        c = chr(c)
+    print("{}".format(c), end='')
