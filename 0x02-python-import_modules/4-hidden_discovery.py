cfcbe3c654e5afdf2ad68c09ee17d3a351a4a38c
MelissaN <majajachiu@yahoo.com>
2018-04-12 22:00:36 +0000

    account for not executing when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-for i in (dir(hidden_4)):
-    if not i.startswith("__"):
-        print("{:s}".format(i))
+if __name__ == "__main__":
+    import hidden_4
+    for i in dir(hidden_4):
+        if i[0] != '_':
+            print("{:s}".format(i))
