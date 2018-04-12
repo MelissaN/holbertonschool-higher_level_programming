251ad27fdfbe88dcf102a7e80232c2e12bfa5768
MelissaN <majajachiu@yahoo.com>
2018-04-12 00:18:23 +0000

    pep dislikes comments?

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-#f.o.r i in range(0, 100):
-#    if i is not 99:
-#        p.r.i.n.t("{:02}, ".format(i), end="")
-#    else:
-#        p.r.i.n.t("{:02}".format(i))
 print(", ".join("{:02d}".format(i) for i in range(0, 100)))
