1a1e430c03ce43a6d8b6de7451fbf1488ae056d2
MelissaN <majajachiu@yahoo.com>
2018-04-12 20:42:23 +0000

    initial commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    if (len(sys.argv) > 1):
+        for i in range(1, len(sys.argv)):
+            result += (int(sys.argv[i]))
+    print("{:d}".format(result))
