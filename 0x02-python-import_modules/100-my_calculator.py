d33e392089cbb583f3b7a79f2c432d2a35f0a395
MelissaN <majajachiu@yahoo.com>
2018-04-12 23:35:50 +0000

    reverse flow of code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,6 +7,14 @@ if __name__ == "__main__":
         print("{:s}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
         exit(1)
 
+    if (argv[2] is not "+" and
+        argv[2] is not "-" and
+        argv[2] is not "*" and
+        argv[2] is not "/"):
+        print("{:s}".format(
+            "Unknown operator. Available operators:+, -, * and /"))
+        exit(1)
+
     print("{:s} {:s} {:s} = ".format(argv[1], argv[2], argv[3]), end="")
     if (argv[2] == "+"):
         print("{:d}".format(add(int(argv[1]), int(argv[3]))))
@@ -14,8 +22,5 @@ if __name__ == "__main__":
         print("{:d}".format(sub(int(argv[1]), int(argv[3]))))
     elif (argv[2] == '*'):
         print("{:d}".format(mul(int(argv[1]), int(argv[3]))))
-    elif (argv[2] == "/"):
-        print("{:d}".format(div(int(argv[1]), int(argv[3]))))
     else:
-        print("{:s}".format("Unknown operator. Available operators: +, -, * and /"))
-        exit(1)
+        print("{:d}".format(div(int(argv[1]), int(argv[3]))))
