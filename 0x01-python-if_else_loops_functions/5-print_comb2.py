6aab0e445e3714810b91688304c9ccc5bf8afa6f
MelissaN <majajachiu@yahoo.com>
2018-04-12 00:03:14 +0000

    alternative method

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i is not 99:
-        print("{:02}, ".format(i), end="")
-    else:
-        print("{:02}".format(i))
+print(", ".join("{:02d}".format(i) for i in range(0, 100)))
