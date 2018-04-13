3f7adcb5c2fe89e3d92bfb90937d1c04bb0a1eee
MelissaN <majajachiu@yahoo.com>
2018-04-13 01:46:23 +0000

    move return out of loop

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,6 +5,6 @@ def magic_calculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-            return c
+        return c
     else:
         return sub(a, b)
