27844a0ec8847c9e8b71522f1d0aa2a83d5b7d31
MelissaN <majajachiu@yahoo.com>
2018-04-13 03:43:12 +0000

    alternative method

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,3 +6,6 @@ if __name__ == "__main__":
         for i in range(1, len(sys.argv)):
             result += (int(sys.argv[i]))
     print("{:d}".format(result))
+
+# import sys
+# print("{:d}".format(sum(int(n) for n in sys.argv[1:])))
