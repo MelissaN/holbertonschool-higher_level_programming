7b2f9c66526b7ed96b08705f3edeb45895914514
MelissaN <majajachiu@yahoo.com>
2018-04-13 00:22:25 +0000

    store argvs in variable

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,18 +8,21 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    if (argv[2] is not "+" and argv[2] is not "-" and
-            argv[2] is not "*" and argv[2] is not "/"):
+    if (argv[2] is not "+" and argv[2] is not "-" and argv[2] is not "*" and
+            argv[2] is not "/"):
         print("{:s}".format(
             "Unknown operator. Available operators:+, -, * and /"))
         exit(1)
 
+    a = int(argv[1])
+    b = int(argv[3])
+
     print("{:s} {:s} {:s} = ".format(argv[1], argv[2], argv[3]), end="")
     if (argv[2] == "+"):
-        print("{:d}".format(add(int(argv[1]), int(argv[3]))))
+        print("{:d}".format(add(a, b)))
     elif (argv[2] == "-"):
-        print("{:d}".format(sub(int(argv[1]), int(argv[3]))))
+        print("{:d}".format(sub(a, b)))
     elif (argv[2] == '*'):
-        print("{:d}".format(mul(int(argv[1]), int(argv[3]))))
+        print("{:d}".format(mul(a, b)))
     else:
-        print("{:d}".format(div(int(argv[1]), int(argv[3]))))
+        print("{:d}".format(div(a, b)))
