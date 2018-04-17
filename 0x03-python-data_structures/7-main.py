168a13d3d11b05fc9f6ca5c7cb9440175d0c0516
MelissaN <majajachiu@yahoo.com>
2018-04-17 23:19:38 +0000

    add test cases, account for bigger tuples

--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -6,5 +6,6 @@ tuple_b = (88, 11)
 new_tuple = add_tuple(tuple_a, tuple_b)
 print(new_tuple)
 
+print(add_tuple(tuple_a, (1, 2, 3, 4, 5)))
 print(add_tuple(tuple_a, (1, )))
 print(add_tuple(tuple_a, ()))
