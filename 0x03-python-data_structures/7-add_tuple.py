11b34e900820efabf7e8845f71387c2d4f3505ce
MelissaN <majajachiu@yahoo.com>
2018-04-17 23:09:14 +0000

    undo use of map and sum builtins

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_b) is 0:
-        return(tuple(map(sum, zip(tuple_a, (0,0)))))
+        return(tuple(i+j for i, j in zip(tuple_a, (0,0))))
     elif len(tuple_b) is 1:
         new = tuple_b + (0,)
-        return(tuple(map(sum, zip(tuple_a, new))))
+        return(tuple(i+j for i, j in zip(tuple_a, new)))
     else:
-        return(tuple(map(sum, zip(tuple_a, tuple_b))))
+        return(tuple(i+j for i, j in zip(tuple_a, tuple_b)))
