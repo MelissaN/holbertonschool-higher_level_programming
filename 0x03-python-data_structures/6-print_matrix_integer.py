931b3a91881ff4b7eb76d39066a91a221fb8c03d
MelissaN <majajachiu@yahoo.com>
2018-04-17 21:41:45 +0000

    initial commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            if i is not row[len(row) - 1]:
+                print ("{:d}".format(i), end=" ")
+            else:
+                print ("{:d}".format(i), end="")
+        print()
