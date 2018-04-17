d8c74e133a045a331e451c03edef1ffd652ab5c9
MelissaN <majajachiu@yahoo.com>
2018-04-17 20:18:44 +0000

    add test cases;return copy of list

--- 0x03-python-data_structures/4-main.py
+++ 0x03-python-data_structures/4-main.py
@@ -8,3 +8,51 @@ new_list = new_in_list(my_list, idx, new_element)
 
 print(new_list)
 print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 0
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 4
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 5
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 6
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = -3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
+
+my_list = [1]
+idx = 1
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    list_copy = my_list[:]
     if idx >= 0 and idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+        list_copy[idx] = element
+    return list_copy
