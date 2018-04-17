f8b4705ada1ab37fabb183847fc7dbedf133e9c1
MelissaN <majajachiu@yahoo.com>
2018-04-17 21:48:35 +0000

    remove space for pep8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
             if i is not row[len(row) - 1]:
-                print ("{:d}".format(i), end=" ")
+                print("{:d}".format(i), end=" ")
             else:
-                print ("{:d}".format(i), end="")
-        print ()
+                print("{:d}".format(i), end="")
+        print()
