8bb623120dc73cc8dda9ecae8aa104401430c08b
MelissaN <majajachiu@yahoo.com>
2018-04-18 00:04:49 +0000

    initial commit

---
+++ 0x03-python-data_structures/8-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+multiple_returns = __import__('8-multiple_returns').multiple_returns
+
+sentence = "At Holberton school, I learnt C!"
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
+
+sentence = ""
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence:
+        return (len(sentence), sentence[0])
+    return (0, None)
