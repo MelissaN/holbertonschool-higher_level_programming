6bcce59c9c8b1061973558786af454383bf1f520
MelissaN <majajachiu@yahoo.com>
2018-04-18 00:18:53 +0000

    initial commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        biggest = 0
+        for i in my_list:
+            if i > biggest:
+                biggest = i
+        return biggest
+    else:
+        return None
