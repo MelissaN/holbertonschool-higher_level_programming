35401b79e59553f3ed4983226be1255b23040d12
MelissaN <majajachiu@yahoo.com>
2018-04-18 01:15:38 +0000

    account for negatives

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -4,3 +4,15 @@ max_integer = __import__('9-max_integer').max_integer
 my_list = [1, 90, 2, 13, 34, 5, -13, 3]
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
+
+my_list = [-91, -90]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
+
+my_list = [1]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
+
+my_list = []
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        biggest = 0
+        biggest = my_list[0]
         for i in my_list:
             if i > biggest:
                 biggest = i
