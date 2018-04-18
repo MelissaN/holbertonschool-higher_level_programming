bc054247de4cd8f58563a162aa510c4474158528
MelissaN <majajachiu@yahoo.com>
2018-04-18 04:08:48 +0000

    pep style

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+
     a = len(tuple_a)
     b = len(tuple_b)
 
     sums = ((tuple_a[0] if a > 0 else 0) + (tuple_b[0] if b > 0 else 0),
            (tuple_a[1] if a > 1 else 0) + (tuple_b[1] if b > 1 else 0))
 
-    return sums
+    return(sums)
