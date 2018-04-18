de95baa985aba952577ffb6d1b988f110401ae41
MelissaN <majajachiu@yahoo.com>
2018-04-18 00:53:53 +0000

    return same list if nothing to delete

--- 0x03-python-data_structures/11-main.py
+++ 0x03-python-data_structures/11-main.py
@@ -6,3 +6,45 @@ idx = 3
 new_list = delete_at(my_list, idx)
 print(new_list)
 print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 0
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = -3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 4
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [1, 2, 3, 4, 5]
+idx = 5
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [1]
+idx = 0
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
+
+my_list = [1]
+idx = 1
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
