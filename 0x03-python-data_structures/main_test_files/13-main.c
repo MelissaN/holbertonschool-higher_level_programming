118a8ebcfc3ac47558ea177a1dc78b83b8d5e0be
MelissaN <majajachiu@yahoo.com>
2018-04-18 06:39:02 +0000

    added test cases

--- 0x03-python-data_structures/main_test_files/13-main.c
+++ 0x03-python-data_structures/main_test_files/13-main.c
@@ -73,6 +73,33 @@ int main(void)
 
 	head = NULL;
 	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+	free_listint(head);
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 1);
+	print_listint(head);
+
+	if (is_palindrome(&head) == 1)
+		printf("Linked list is a palindrome\n");
+	else
+		printf("Linked list is not a palindrome\n");
+	free_listint(head);
+
+	head = NULL;
+	add_nodeint_end(&head, 1);
 	add_nodeint_end(&head, 17);
 	add_nodeint_end(&head, 500000);
 	print_listint(head);
