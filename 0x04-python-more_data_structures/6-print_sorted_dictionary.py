bcfe29c5ff0a254630c3bc0f271efcc5c0519799
MelissaN <majajachiu@yahoo.com>
2018-04-19 21:42:06 +0000

    fix pep8 style

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     if a_dictionary is not None:
         for key, value in sorted(a_dictionary.items()):
-            print ("{}: {}".format(key, value))
+            print("{}: {}".format(key, value))
     else:
         return
