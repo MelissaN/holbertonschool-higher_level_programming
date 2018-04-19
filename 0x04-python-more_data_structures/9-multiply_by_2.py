6b0b1281890a09a223fd0a49b4ca9dd67ef1539b
MelissaN <majajachiu@yahoo.com>
2018-04-19 21:44:10 +0000

    fix pep8 style

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     if a_dictionary is not None:
-        dict = { }
-        tmp = { }
+        dict = {}
+        tmp = {}
         for key, value in a_dictionary.items():
             newval = value * 2
             tmp = {key: newval}
