cc5b94b52e70ca8f0aa8dd5dfb871b3424c1a867
MelissaN <majajachiu@yahoo.com>
2018-04-19 23:57:40 +0000

    initial commit

---
+++ 0x04-python-more_data_structures/12-main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Roman to Integer test file
+"""
+roman_to_int = __import__('12-roman_to_int').roman_to_int
+
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = ""
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "ACC"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "AZZ"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "XX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = 123
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "123"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = None
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "I"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string:
+        if roman_string is not str(roman_string):
+            return(0)
+
+        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        symbols = list(roman_string)
+        for s in symbols:
+            if s not in dic.keys():
+                return(0)
+
+        decode = []
+        decode = [dic.get(x) for x in symbols]
+
+        sum = 0
+        i = 0
+        while(i < len(decode)):
+            if decode[i] == decode[-1]:
+                sum += decode[i]
+            elif decode[i] < decode[i + 1]:
+                sum -= decode[i]
+            else:
+                sum += decode[i]
+            i += 1
+        return(sum)
+    else:
+        return(0)
