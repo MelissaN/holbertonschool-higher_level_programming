7901728f2fca384df0c71914383d3bf1fe4d8c4b
MelissaN <majajachiu@yahoo.com>
2018-04-20 00:47:36 +0000

    remove else

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if a_dictionary:
         return(max(a_dictionary, key=a_dictionary.get))
-    else:
-        return None
