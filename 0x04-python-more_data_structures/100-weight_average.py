7d4a743c4f6a3bf5ad9446fb24e063ce68658f5c
MelissaN <majajachiu@yahoo.com>
2018-04-20 01:16:26 +0000

    return 0 for empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    total = 0
-    frequency = 0
-    for tup in my_list:
-        (weight, occurence) = tup
-        total += (weight * occurence)
-        frequency += occurence
-    return (total/frequency)
+    if my_list is not None:
+        total = 0
+        frequency = 0
+        for tup in my_list:
+            (weight, occurence) = tup
+            total += (weight * occurence)
+            frequency += occurence
+        return (total/frequency)
+    else:
+        return (0)
