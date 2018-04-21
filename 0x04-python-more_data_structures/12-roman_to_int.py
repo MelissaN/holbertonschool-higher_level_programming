48718bd9c8a99d3e4777e62336bbf75c637defc1
MelissaN <majajachiu@yahoo.com>
2018-04-21 03:35:30 +0000

    switch to cleaner method with zip

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string:
-        if roman_string is not str(roman_string):
-            return(0)
-
-        dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        symbols = list(roman_string)
-        for s in symbols:
-            if s not in dic.keys():
-                return(0)
-
-        decode = []
-        decode = [dic.get(x) for x in symbols]
-
-        sum = 0
-        i = 0
-        while(i < len(decode)):
-            if i is (len(decode) - 1):
-                sum += decode[i]
-            elif decode[i] < decode[i + 1]:
-                sum -= decode[i]
-            else:
-                sum += decode[i]
-            i += 1
-        return(sum)
-    else:
-        return(0)
+    if roman_string is None or type(roman_string) is not str:
+        return 0
+    if roman_string == "":
+        return 0
+    num = 0
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for i, j in zip(roman_string, roman_string[1:]):
+        if i not in dic.keys():
+            return 0
+        elif dic[i] >= dic[j]:
+            num += dic[i]
+        else:
+            num -= dic[i]
+    num += dic[roman_string[-1]]
+    return num
