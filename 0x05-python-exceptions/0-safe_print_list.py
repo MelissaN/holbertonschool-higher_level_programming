777d7216f351cf3886f39c60662d7fe86627dd3e
MelissaN <majajachiu@yahoo.com>
2018-04-30 23:11:38 +0000

    pep8 doesn't like extra line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     idx = 0
     while idx < x:
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     printed = 0
     for idx in range(x):
