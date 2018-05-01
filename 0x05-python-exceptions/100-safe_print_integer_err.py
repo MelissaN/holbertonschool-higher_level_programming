97529e939b2d5db6d6bdd9dd399dff77220e4ff5
MelissaN <majajachiu@yahoo.com>
2018-05-01 05:08:04 +0000

    forgot exclamation in shebang; revert original

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,10 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 from sys import stderr
 
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
+        return (True)
     except (TypeError, ValueError) as e:
         stderr.write("Exception: {}\n".format(e))
         return (False)
-    else:
-        return (True)
