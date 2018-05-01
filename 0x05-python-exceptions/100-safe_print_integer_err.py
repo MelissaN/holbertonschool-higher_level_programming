e7939a895993b1bd4eea1146e1d718ad792ca62f
MelissaN <majajachiu@yahoo.com>
2018-05-01 05:03:56 +0000

    rewrite import, try else statement

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,11 @@
 #/usr/bin/python3
-import sys
+from sys import stderr
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as e:
-        sys.stderr.write("Exception: {}\n".format(e))
+        stderr.write("Exception: {}\n".format(e))
         return (False)
-    return (True)
+    else:
+        return (True)
