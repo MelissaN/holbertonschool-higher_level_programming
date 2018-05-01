c131c047536717482bcc7d7feb5f86ae54d434fc
MelissaN <majajachiu@yahoo.com>
2018-05-01 00:57:18 +0000

    reattempt with return True at end

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except (TypeError, ValueError) as e:
         sys.stderr.write("Exception: {}\n".format(e))
         return False
+    return True
