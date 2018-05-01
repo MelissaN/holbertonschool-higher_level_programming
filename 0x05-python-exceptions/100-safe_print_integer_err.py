7af9df3779f7f12f08e936bb0728c29d04042042
MelissaN <majajachiu@yahoo.com>
2018-05-01 01:00:58 +0000

    add parenthesis to return T/F

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
     except (TypeError, ValueError) as e:
         sys.stderr.write("Exception: {}\n".format(e))
-        return False
-    return True
+        return (False)
+    return (True)
