3959beeada6fc638cde5b41e94d6b125123e691a
MelissaN <majajachiu@yahoo.com>
2018-05-02 05:34:54 +0000

    write class to match bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define MagicClass"""
+import math
+
+
+class MagicClass:
+    """Initialize and define methods area and circumference"""
+    def __init__(self, radius=0):
+        """Initialize MagicClass"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        """Calculate area"""
+        self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calcualte circumference"""
+        return 2 * math.pi * self.__radius
+
+#import dis
+#dis.dis(MagicClass)
