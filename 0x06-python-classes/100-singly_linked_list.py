9019da5a18256d1fac834b8ba29c09b01872479b
MelissaN <majajachiu@yahoo.com>
2018-05-02 02:38:06 +0000

    initial commit

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python3
+"""
+Module 100-singly_linked_list
+Defines class Node (with private data and next_node)
+Defines class SinglyLinkedList (with private head and public sorted_insert)
+"""
+
+
+class Node:
+    """
+    class Node definition
+
+    Args:
+        data (int): private
+        next_node : private; can be None or Node object
+
+    Functions:
+        __init__(self, data, next_node=None)
+        data(self)
+        data(self, value)
+        next_node(self)
+        next_node(self, value)
+    """
+
+    def __init__(self, data, next_node=None):
+        """
+        Initializes node
+
+        Attributes:
+            data (int): private
+            next_node : private; can be None or Node object
+        """
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        """"
+        Getter
+
+        Return: data
+        """
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets data to value if int
+        """
+        if type(value) is not int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        """"
+        Getter
+
+        Return: next_node
+        """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets next_node if value is next_node or None
+        """
+        if type(value) is not Node and value is not None:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+class SinglyLinkedList:
+    """
+    class SinglyLinkedList definition
+
+    Args:
+        head: private
+
+    Functions:
+        __init__(self)
+        sorted_insert(self, value)
+    """
+
+    def __init__(self):
+        """
+        Initializes singly linked list
+
+        Attributes:
+            head: private
+        """
+        self.__head = None
+
+    def __str__(self):
+        """
+        String representation of singly linked list needed to print
+        """
+        string = ""
+        tmp = self.__head
+        while tmp is not None:
+            string += str(tmp.data)
+            tmp = tmp.next_node
+            if tmp is not None:
+                string += "\n"
+        return string
+
+    def sorted_insert(self, value):
+        """
+        Inserts new nodes into singly linked list in sorted order
+
+        Args:
+        value: int data for node
+        """
+        new = Node(value)
+        if self.__head is None:
+            self.__head = new
+            return
+
+        tmp = self.__head
+        if new.data < tmp.data:
+            new.next_node = self.__head
+            self.__head = new
+            return
+
+        while (tmp.next_node is not None) and (new.data > tmp.next_node.data):
+            tmp = tmp.next_node
+        new.next_node = tmp.next_node
+        tmp.next_node = new
+        return
---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+"""
+Module 101-square
+Defines class Square with private size & position; and public area & my_print
+Can access and update size and position
+Can print to stdout the square using #'s
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size (int): size of a side in square
+
+    Functions:
+        __init__(self, size, position)
+        size(self)
+        size(self, value)
+        position(self)
+        position(self, value)
+        area(self)
+        my_print(self)
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Initializes square
+
+        Attributes:
+            size (int): defaults to 0 if none; don't use __size to call setter
+            position (int): tuple of two positive integers
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """"
+        Getter
+
+        Return: size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets size to value if int and >= 0
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        """"
+        Getter
+
+        Return: position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Setter
+
+        Args:
+            value: sets position to tuple if value is tuple of 2 positive ints
+        """
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """
+        Calculates area of square
+
+        Returns:
+            area
+        """
+        return (self.__size)**2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            print("\n" * self.__position[1], end="")
+            print("\n".join([" " * self.__position[0] +
+                             "#" * self.__size
+                             for rows in range(self.__size)]))
+
+    def __str__(self):
+        """
+        String representation of square so call to print works
+        Example: print(my_square)
+        """
+        string = ""
+        if self.__size == 0:
+            return string
+
+        string += "\n" * self.position[1]
+        string += "\n".join([" " * self.__position[0] +
+                             "#" * self.__size
+                             for rows in range(self.__size)])
+        return string
