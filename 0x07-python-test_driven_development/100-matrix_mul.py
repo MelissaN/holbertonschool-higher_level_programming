e51dc49624d7d2489158f31a4214d917891f27e6
MelissaN <majajachiu@yahoo.com>
2018-05-07 02:35:21 +0000

    reorder checks

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -14,7 +14,7 @@ def matrix_mul(m_a, m_b):
         raise TypeError("{} must be a list".format
                         ("m_a" if not isinstance(m_a, list) else "m_b"))
 
-    if len(m_a) == 0 or len(m_b) == 0:
+    if len(m_a) == 0 or len(m_b) == 0 or m_a == [[]] or m_b == [[]]:
         raise ValueError("{} can't be empty".format
                          ("m_a" if len(m_a) == 0 else "m_b"))
 
@@ -22,10 +22,10 @@ def matrix_mul(m_a, m_b):
         for n in eachrow:
             if not isinstance(n, (int, float)):
                 raise TypeError("m_a should contain only integers or floats")
-        if len(eachrow) != len(m_b):
-            raise ValueError("m_a and m_b can't be multiplied")
         if len(eachrow) != len(m_a[0]):
             raise TypeError("each row of m_a must should be of the same size")
+        if len(eachrow) != len(m_b):
+            raise ValueError("m_a and m_b can't be multiplied")
 
     for eachrow in m_b:
         for n in eachrow:
