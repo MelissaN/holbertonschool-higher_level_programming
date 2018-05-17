d81f543ebcb4c69a457f4a7fbbe48487ad09cbfa
MelissaN <majajachiu@yahoo.com>
2018-05-17 04:49:18 +0000

    add tests and initial code

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -19,7 +19,14 @@ def pascal_triangle(n):
             [1, 5, 10, 10, 5, 1]
             [1, 6, 15, 20, 15, 6, 1]
     """
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
 
-
+    triangle = [[1]]
+    for rows in range(n-1):
+        triangle.append([a+b for a, b
+                         in zip([0] + triangle[-1], triangle[-1] + [0])])
 
     return triangle
--- 0x0B-python-input_output/main_files/14-main.py
+++ 0x0B-python-input_output/main_files/14-main.py
@@ -13,4 +13,15 @@ def print_triangle(triangle):
 
 
 if __name__ == "__main__":
+    print("Size -5 shouldn't print anything")
+    print_triangle(pascal_triangle(-5))
+    print("Size 0 shouldn't print anything")
+    print_triangle(pascal_triangle(0))
+    print("Size 1 should print [1]")
+    print_triangle(pascal_triangle(1))
+    print("Size 2 should print [1] , [1, 1]")
+    print_triangle(pascal_triangle(2))
+    print("Size 3 should print [1], [1, 1], [1, 2, 1]")
+    print_triangle(pascal_triangle(3))
+    print("Size 5 should print [1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]")
     print_triangle(pascal_triangle(5))
