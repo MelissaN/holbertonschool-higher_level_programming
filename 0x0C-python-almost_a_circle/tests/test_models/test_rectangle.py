07b92dd39ea6fe1cba44e89066a14e6682efeef0
MelissaN <majajachiu@yahoo.com>
2018-05-22 00:17:39 +0000

    rename test to tests, comment how to run

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 Unittest for Rectangle Class
+# run with python3 -m unittest discover tests
+# run with python3 -m unittest tests/test_models/test_rectangle.py
 """
 
 import unittest
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 Unittest for Square Class
+# run with python3 -m unittest discover tests
+# run with python3 -m unittest tests/test_models/test_square.py
 """
 
 import unittest
