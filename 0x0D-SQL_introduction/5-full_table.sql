51171647977480c08cfc22492551812000c95117
MelissaN <majajachiu@yahoo.com>
2018-06-26 01:50:01 +0000

    show table info

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+-- print full description of table; without using 'DECSCRIBE [tableName]'
+--
+-- Table   Create Table
+-- first_table CREATE TABLE `first_table` (\n  `id` int(11) DEFAULT NULL,
+-- \n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+SHOW CREATE TABLE first_table;
