1537f4114a9deba163ac0873eacca246bab12c27
MelissaN <majajachiu@yahoo.com>
2018-06-27 02:10:08 +0000

    joining tables and summing data to display

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,17 @@
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows_rate.sql
+-- run this first to import a SQL dump -->
+--      echo "CREATE DATABASE hbtn_0d_tvshows_rate;" | mysql -uroot -p
+--      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows_rate
+--
+-- script that lists all shows from hbtn_0d_tvshows_rate by their rating
+-- each record should display: tv_shows.title - rating sum
+-- results must be sorted in descending order by the rating
+-- can use only one SELECT statement
+-- the database name will be passed as an argument of the mysql command
+
+    SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
+      FROM tv_shows
+      JOIN tv_show_ratings
+        ON tv_shows.id = tv_show_ratings.show_id
+  GROUP BY title
+  ORDER BY rating DESC;
---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,19 @@
+-- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows_rate.sql
+-- run this first to import a SQL dump -->
+--      echo "CREATE DATABASE hbtn_0d_tvshows_rate;" | mysql -uroot -p
+--      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows_rate
+--
+-- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating
+-- each record should display: tv_genres.name - rating sum
+-- results must be sorted in descending order by the rating
+-- can use only one SELECT statement
+-- the database name will be passed as an argument of the mysql command
+
+    SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
+      FROM tv_genres
+      JOIN tv_show_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+      JOIN tv_show_ratings
+        ON tv_show_ratings.show_id = tv_show_genres.show_id
+  GROUP BY name
+  ORDER BY rating DESC;
