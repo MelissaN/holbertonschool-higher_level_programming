a97d4a3bb619134ca160bbc22da38127f7327614
Melissa Ng <32788311+MelissaN@users.noreply.github.com>
2018-06-27 10:54:06 -0700

    change join to inner join

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -11,7 +11,7 @@
 
     SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
       FROM tv_shows
-      JOIN tv_show_ratings
+INNER JOIN tv_show_ratings
         ON tv_shows.id = tv_show_ratings.show_id
   GROUP BY title
   ORDER BY rating DESC;
