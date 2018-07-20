c3800b6cc2cb2d6526bf9c31dbbad61bb8e44d0b
MelissaN <majajachiu@yahoo.com>
2018-07-20 22:03:40 +0000

    add City python class; script to print from tables

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+return all cities from database via python
+parameters given to script: username, password, database
+"""
+
+from sys import argv
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # query multiple tables in database and print info from tables
+    for q in session.query(State.name, City.id, City.name).filter(
+            State.id == City.state_id).order_by(City.id):
+        print("{:s}: ({:d}) {:s}".format(q[0], q[1], q[2]))
+
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Defines class City
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+
+class City(Base):
+    """
+    Class City; instance of Base
+    Linked to MySQL table "city"
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, nullable=False, primary_key=True) #autoincrements
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
