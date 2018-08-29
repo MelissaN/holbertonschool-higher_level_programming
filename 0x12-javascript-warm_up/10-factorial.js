d08cd3b086b8bd8d7e1f513f1f1c74182447dbd3
MelissaN <majajachiu@yahoo.com>
2018-08-29 00:38:56 +0000

    10- add script with factorial function

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const n = parseInt(process.argv[2], 10);
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return (n * factorial (n - 1));
+}
+console.log(factorial(n));
