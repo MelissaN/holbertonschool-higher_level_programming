62b22759384735329aace516a64524774a4cffcb
MelissaN <majajachiu@yahoo.com>
2018-08-29 01:05:21 +0000

    style variables to camelCase

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const nums_array = process.argv.slice(2);
-function second_max (array) {
+const numsArray = process.argv.slice(2);
+function secondMax (array) {
   if (array.length < 2) {
     return (0);
   } else {
@@ -10,4 +10,4 @@ function second_max (array) {
     return (array.pop());
   }
 }
-console.log(second_max(nums_array));
+console.log(secondMax(numsArray));
