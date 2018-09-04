95bd30d91a1c0245837091316e19756a632de92f
MelissaN <majajachiu@yahoo.com>
2018-09-04 22:50:12 +0000

    add rectangle class w/ width/height and rotate/double/print methods

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let col = 0; col < this.height; col += 1) {
+      console.log("X".repeat(this.width));
+    }
+  }
+};
---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print() {
+    for (let col = 0; col < this.height; col += 1) {
+      console.log("X".repeat(this.width));
+    }
+  }
+  rotate() {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
