39f8555ca1d2ce09836ee38972bb0a40e18ffd28
MelissaN <majajachiu@yahoo.com>
2018-09-04 23:14:11 +0000

    semistandard wants space between func/paren and single quote for strings

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,7 +7,7 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-  print() {
+  print () {
     for (let col = 0; col < this.height; col += 1) {
       console.log("X".repeat(this.width));
     }
