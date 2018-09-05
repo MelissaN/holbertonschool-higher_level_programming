3641db197d5cb208efd51bac55383dd3daf106df
MelissaN <majajachiu@yahoo.com>
2018-09-05 21:02:28 +0000

    add script to create dict based on completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+let url = process.argv[2];
+const request = require('request');
+
+request(url, function(err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    dic = {};
+    let tasks = JSON.parse(body);
+    for (let i in tasks) {
+      if (dic[tasks[i].userId] === undefined) {
+	dic[tasks[i].userId] = 0;
+      }
+      if (tasks[i].completed) {
+	dic[tasks[i].userId]++;
+      }
+    }
+    console.log(dic);
+  } else {
+    console.log("Error code: " + response.statusCode);
+  }
+});
