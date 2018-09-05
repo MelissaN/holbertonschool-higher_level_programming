d01892887edce16464b57201c28e045d51898fba
MelissaN <majajachiu@yahoo.com>
2018-09-05 21:19:18 +0000

    semistandard forgot space

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 let url = process.argv[2];
 const request = require('request');
 
-request(url, function(err, response) {
+request(url, function (err, response) {
   if (err) {
     console.log(err);
   } else {
