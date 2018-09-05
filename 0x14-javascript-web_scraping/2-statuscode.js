f46bcf4c6ae83598a551abae5f80aa32f570da4a
MelissaN <majajachiu@yahoo.com>
2018-09-05 18:54:00 +0000

    add script to request url's status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let url = process.argv[2];
+const request = require('request');
+
+request(url, function(err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
