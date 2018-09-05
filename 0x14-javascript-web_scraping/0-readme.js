9cd855c4b8145fa43b97cacc6f813bcedd2dc040
MelissaN <majajachiu@yahoo.com>
2018-09-05 18:47:25 +0000

    add scripts to read and write from/to filename

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+let filename = process.argv[2];
+const fs = require('fs');
+fs.readFile(filename, 'utf8', function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+let filename = process.argv[2];
+let text = process.argv[3];
+const fs = require('fs');
+fs.writeFile(filename, text, 'utf8', function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
