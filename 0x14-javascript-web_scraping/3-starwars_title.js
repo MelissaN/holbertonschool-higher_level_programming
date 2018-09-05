ab3ba7bbf9b758e2f650c0b971a523b33d5f3fe2
MelissaN <majajachiu@yahoo.com>
2018-09-05 19:10:29 +0000

    add script to request starwars api title info

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+let id = process.argv[2];
+let url = 'http://swapi.co/api/films/' + id;
+const request = require('request');
+
+request(url, function(err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body['title']);
+  } else {
+    console.log("Erorr Code:" + response.statusCode);
+  }
+});
