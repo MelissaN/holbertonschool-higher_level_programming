8797699f7c1f57e550fb1282f5b2c2d0e788b93f
MelissaN <majajachiu@yahoo.com>
2018-09-05 20:39:41 +0000

    add script to count occurences in api

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+let url = process.argv[2];
+const request = require('request');
+
+request(url, function(err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    let films = JSON.parse(body).results;
+    let count = 0;
+    for (let i in films) {
+      let chars = films[i].characters;
+      for (let c in chars) {
+	if (chars[c].includes('18')) {
+	  count++;
+	}
+      }
+    }
+    console.log(count);
+  } else {
+    console.log("Erorr Code:" + response.statusCode);
+  }
+});
