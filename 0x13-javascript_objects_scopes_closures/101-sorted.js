d5a029b2d86841b2518ce929cc959e8c9b087d3e
MelissaN <majajachiu@yahoo.com>
2018-09-05 05:54:09 +0000

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,7 +5,7 @@ const dict = require('./101-data').dict;
 let newDic = {};
 for (let key in dict) {
   if (newDic[dict[key]] === undefined) {
-    newDic[dict[key]] = []
+    newDic[dict[key]] = [];
   }
   newDic[dict[key]].push(key);
 }
